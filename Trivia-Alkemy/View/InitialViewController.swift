8af879584f0d463ff82540fb01096bee10316810
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-05-19 09:43:55 -0300

    Set InitialVC

---
+++ Trivia-Alkemy/View/InitialViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InitialViewController.swift
+//  Trivia-Alkemy
+//
+//  Created by Santiago Rodriguez Affonso on 19/05/2022.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
