08d22d7db39be1f3fe49ec5292db080a20e0193c
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-05-19 09:16:49 -0300

    Initial Commit

---
+++ Trivia-Alkemy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trivia-Alkemy
+//
+//  Created by Santiago Rodriguez Affonso on 19/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
